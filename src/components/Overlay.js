56cfe6f286b8c22f7f72ba9b62134bf808939f33
portareset1 <57542832+portareset1@users.noreply.github.com>
2022-02-05 11:30:15 -0400

    modified props and display

--- src/components/Overlay.js
+++ src/components/Overlay.js
@@ -1,17 +1,27 @@
 import "../styles/Overlay.css";
 import React from "react";
 import Loader from "./Loader";
-const Overlay = ({ text }) => {
+const Overlay = ({
+  level,
+  score,
+  showLoader,
+  showGameOverInfo,
+  onButtonClicked,
+}) => {
   return (
     <div className="Overlay">
-      <Loader text={text}></Loader>
-      {/*
+      {showLoader && <Loader level={level}></Loader>}
+      {showGameOverInfo && (
         <div className="Overlay__content">
           <h1 className="Overlay__title">Game Over</h1>
-          <p className="Overlay__text">Congratulations, your score is 1</p>
-          <button className="Overlay__button">New Game</button>
+          <p className="Overlay__text">
+            Congratulations, your score is {score}
+          </p>
+          <button onClick={onButtonClicked} className="Overlay__button">
+            New Game
+          </button>
         </div>
-      */}
+      )}
     </div>
   );
 };
